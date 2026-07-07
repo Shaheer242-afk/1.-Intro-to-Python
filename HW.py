e13d4aa76a3c0850bce09e2870e18d84d4b1535c
Shaheer242-afk <shaheersyed.s_waa@gemsed.com>
2026-07-07 16:13:09 +0400

    Add files via upload

---
+++ HW.py
@@ -0,0 +1,25 @@
+print("My Daily Routine")
+
+# Step 1: Waking up
+print("I wake up at 6 AM.")
+
+# Step 2: Brushing my teeth and having breakfast
+print("I brush my teeth and eat my breakfast by around 6:30")
+
+# Step 3: Taking the bus
+print("I take the bus to school at 6:50 AM.")
+
+# Step 4: School Starts
+print("School starts at 7:30 AM.")
+
+# Step 5: Lunch after school
+print("After School, I eat my luch at around 4 PM.")
+
+# Step 6: Studying
+print("I study for 2 hours after school and do my homework.")
+
+# Step 7: Fifa World Cup
+print("I Watch a world cup match if it is starts at 8 or 9 PM")
+
+# Calculating total study time in a week
+print("Total study hours in a week:", 2 * 7)
